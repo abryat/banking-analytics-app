b2168241997a343b5eeaa8b33f85209310e2b608
abryat <ajbatkinson@gmail.com>
2025-02-20 19:09:07 +0000

    implement analysis setup form

--- client/src/components/AnalysisSetup.vue
+++ client/src/components/AnalysisSetup.vue
@@ -1,3 +1,333 @@
 <template>
-  <h1>Setup Page</h1>
+  <el-form ref="formRef" :model="formData" :rules="formRules" label-width="120px" size="large" status-icon>
+    <h1>Configure Spending Analysis</h1>
+    <h2>Select the parameters for your analysis and click 'Analyse' to view your spending report.</h2>
+
+    <h3>Step 1: Select a date range</h3>
+    <p>Start by choosing a date range for the analysis.</p>
+    <p>Once you have selected a valid date range, additional options to filter by category, sub-category, and account will appear.</p>
+    <el-form-item label="Start Date" prop="startDate">
+      <el-date-picker
+        v-model="formData.startDate"
+        type="date"
+        placeholder="Pick a start date"
+        :disabled-date="disabledDates"
+        format="DD/MM/YYYY"
+        size="large"
+      />
+    </el-form-item>
+
+    <el-form-item label="End Date" prop="endDate">
+      <el-date-picker
+        v-model="formData.endDate"
+        type="date"
+        placeholder="Pick an end date"
+        :disabled-date="disabledDates"
+        format="DD/MM/YYYY"
+        size="large"
+      />
+    </el-form-item>
+
+    <div v-if="dateRangeValid">
+      <h3>Step 2: Select optional filters</h3>
+      <p>Choose the categories, sub-categories, and accounts you would like to include in your analysis.</p>
+      <p>If you prefer, you can skip the filters - all transactions within your chosen date range will be included by default.</p>
+      <el-form-item label="Categories" prop="selectedCategories">
+        <el-select
+          v-model="formData.selectedCategories"
+          multiple
+          filterable
+          clearable
+          placeholder="Select Categories"
+        >
+          <template #header>
+            <el-checkbox
+              v-model="checkAllCategories"
+              :indeterminate="indeterminateCategories"
+              @change="toggleCheckAllCategories"
+            >
+              All
+            </el-checkbox>
+          </template>
+          <el-option
+            v-for="(category, index) in categories"
+            :key="index"
+            :value="category"
+          />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="Sub-categories" prop="selectedSubCategories">
+        <el-select
+          v-model="formData.selectedSubCategories"
+          multiple
+          filterable
+          clearable
+          placeholder="Select sub-category"
+        >
+          <template #header>
+            <el-checkbox
+              v-model="checkAllSubCategories"
+              :indeterminate="indeterminateSubCategories"
+              @change="toggleCheckAllSubCategories"
+            >
+              All
+            </el-checkbox>
+          </template>
+          <el-option
+            v-for="(subCategory, index) in subCategories"
+            :key="index"
+            :value="subCategory"
+          />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="Accounts" prop="selectedAccounts">
+        <el-select
+          v-model="formData.selectedAccounts"
+          multiple
+          filterable
+          clearable
+          placeholder="Select Accounts"
+        >
+          <template #header>
+            <el-checkbox
+              v-model="checkAllAccounts"
+              :indeterminate="indeterminateAccounts"
+              @change="toggleCheckAllAccounts"
+            >
+              All
+            </el-checkbox>
+          </template>
+          <el-option
+            v-for="(account, index) in accounts"
+            :key="index"
+            :value="account"
+          />
+        </el-select>
+      </el-form-item>
+    </div>
+
+    <el-form-item>
+        <el-button type="primary" :disabled="!dateRangeValid" @click="onAnalyse(formRef)">
+          Analyse
+        </el-button>
+    </el-form-item>
+  </el-form>
 </template>
+
+<script lang='ts'>
+import { defineComponent, computed, ref, watch, reactive} from 'vue';
+import { useStore } from 'vuex';
+import moment from 'moment';
+import { Transaction, AnalysisConfig } from '../store/VuexTransactionStore';
+import { useRouter } from 'vue-router';
+import type { FormInstance, FormRules, CheckboxValueType } from 'element-plus';
+import { ElMessage } from 'element-plus';
+
+export default defineComponent({
+  name: 'AnalysisSetup',
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+
+    //Initialise form
+    const formRef = ref<FormInstance>();
+
+    const checkAllCategories = ref(false);
+    const indeterminateCategories = ref(false);
+    const checkAllSubCategories = ref(false);
+    const indeterminateSubCategories = ref(false);
+    const checkAllAccounts = ref(false);
+    const indeterminateAccounts = ref(false);
+
+    const formData = reactive<AnalysisConfig>({
+      startDate: undefined,
+      endDate: undefined,
+      selectedCategories: [],
+      selectedAccounts: [],
+      selectedSubCategories: [],
+    });
+
+    //Form validation rules for required date range selectors
+    const formRules = reactive<FormRules<AnalysisConfig>>({
+      startDate: [
+        {required: true, message: 'Please enter a start date', trigger: 'change'},
+      ],
+      endDate: [
+        {required: true, message: 'Please enter an end date', trigger: 'change'},
+        {validator: validateDateRange, trigger: 'change'},
+      ],
+    });
+
+    //Compute maximum date range from all transactions
+    const transactionDateRange = computed(() => store.getters.getTransactionDateRange);
+
+    //Filter transactions based on date range selected
+    const selectedTransactions = computed<Transaction[]>(() => {
+      if (!formData.startDate || !formData.endDate) {
+        return [];
+      }
+      return store.getters.getTransactionsByDate(formData.startDate, formData.endDate);
+    });
+
+    //Use date-filtered transactions to compute optional dropdown items
+    const categories = computed(() => {
+      return [...new Set(selectedTransactions.value.map((transaction: Transaction) => transaction.category))];
+    });
+
+    const subCategories = computed(() => {
+      const filteredTransactions = selectedTransactions.value.filter((transaction) => formData.selectedCategories.includes(transaction.category));
+      return [...new Set(filteredTransactions.map((transaction: Transaction) => transaction.subCategory))];
+    });
+
+    const accounts = computed(() => {
+      return [...new Set(selectedTransactions.value.map((transaction: Transaction) => transaction.accountName))];
+    });
+
+    const dateRangeValid = computed(() => {
+      return formData.startDate && formData.endDate && formData.startDate < formData.endDate;
+    });
+
+    //Handle individual selection and select all functionality in dropdown menus
+    watch (formData.selectedCategories, (newSelectedCategories: string[]) => {
+      if (newSelectedCategories.length === 0) {
+        checkAllCategories.value = false;
+        indeterminateCategories.value = false;
+      } else if (newSelectedCategories.length === categories.value.length) {
+        checkAllCategories.value = true;
+        indeterminateCategories.value = false;
+      } else {
+        indeterminateCategories.value = true;
+      }
+    });
+
+    watch (formData.selectedSubCategories, (newSelectedSubCategories: string[]) => {
+      if (newSelectedSubCategories.length === 0) {
+        checkAllSubCategories.value = false;
+        indeterminateSubCategories.value = false;
+      } else if (newSelectedSubCategories.length === subCategories.value.length) {
+        checkAllSubCategories.value = true;
+        indeterminateSubCategories.value = false;
+      } else {
+        indeterminateSubCategories.value = true;
+      }
+    });
+
+    watch (formData.selectedAccounts, (newSelectedAccounts: string[]) => {
+      if (newSelectedAccounts.length === 0) {
+        checkAllAccounts.value = false;
+        indeterminateAccounts.value = false;
+      } else if (newSelectedAccounts.length === accounts.value.length) {
+        checkAllAccounts.value = true;
+        indeterminateAccounts.value = false;
+      } else {
+        indeterminateAccounts.value = true;
+      }
+    });
+
+    //Reset filters if date range is changed
+    watch([formData.startDate, formData.endDate], () => {
+      formData.selectedCategories = [];
+      formData.selectedSubCategories = [];
+      formData.selectedAccounts = [];
+      checkAllCategories.value = false;
+      checkAllSubCategories.value = false;
+      checkAllAccounts.value = false;
+      indeterminateCategories.value = false;
+      indeterminateCategories.value = false;
+      indeterminateAccounts.value = false;
+    });
+
+    //Disable dates in the date range selector if they are outside the maximum transaction date range
+    function disabledDates(date: Date) {
+      const calendarDate = moment(date).startOf('day');
+      const startDate = moment(transactionDateRange.value.start).startOf('day');
+      const endDate = moment(transactionDateRange.value.end).startOf('day');
+      return !calendarDate.isBetween(startDate, endDate);
+    }
+
+    //Disable analyse button if date range is invalid
+    function validateDateRange(rule: any, endDate: Date, callback: (error?: string | Error) => void) {
+      if (formData.startDate && new Date(formData.startDate) >= new Date(endDate)) {
+        callback(new Error('Start date must be before end date'));
+      } else {
+        callback();
+      }
+    }
+
+    //Update filters if 'All' is selected or deselected
+    function toggleCheckAllCategories(isChecked: CheckboxValueType) {
+      indeterminateCategories.value = false;
+      if (isChecked) {
+        formData.selectedCategories = categories.value;
+      } else {
+        formData.selectedCategories = [];
+      }
+    }
+
+    function toggleCheckAllSubCategories(isChecked: CheckboxValueType) {
+      indeterminateSubCategories.value = false;
+      if (isChecked) {
+        formData.selectedSubCategories = subCategories.value;
+      } else {
+        formData.selectedSubCategories = [];
+      }
+    }
+
+    function toggleCheckAllAccounts(isChecked: CheckboxValueType) {
+      indeterminateAccounts.value = false;
+      if (isChecked) {
+        formData.selectedAccounts = accounts.value;
+      } else {
+        formData.selectedAccounts = [];
+      }
+    }
+
+    //On form validation/submission, commit the analysis config and route to the analysis page
+    function onAnalyse(form: FormInstance | undefined) {
+      if (!form) return;
+      form.validate((valid) => {
+        if (valid) {
+          store.dispatch('updateAnalysisConfig', formData); //commit analysis config to store
+          ElMessage({
+            message: 'Analysis configuration saved. Generating Analysis...',
+            type: 'success',
+            duration: 2000,
+          });
+          setTimeout(() => {
+            router.push('/analysis'); //route to analysis page
+          }, 2000);
+        } else {
+          ElMessage.error('Please resolve the displayed errors and try again.');
+        }
+      });
+    }
+
+    return {
+      formRef,
+      formData,
+      formRules,
+      checkAllCategories,
+      checkAllSubCategories,
+      checkAllAccounts,
+      indeterminateCategories,
+      indeterminateSubCategories,
+      indeterminateAccounts,
+
+      transactionDateRange,
+      selectedTransactions,
+      categories,
+      subCategories,
+      accounts,
+      dateRangeValid,
+
+      disabledDates,
+      toggleCheckAllCategories,
+      toggleCheckAllSubCategories,
+      toggleCheckAllAccounts,
+      onAnalyse,
+    };
+  },
+});
+</script>
--- client/src/store/VuexTransactionStore.ts
+++ client/src/store/VuexTransactionStore.ts
@@ -13,25 +13,40 @@ export interface Transaction {
   value: number;
 }
 
+export interface AnalysisConfig {
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+  selectedCategories: string[];
+  selectedSubCategories: string[];
+  selectedAccounts: string[];
+}
+
 interface State {
   transactions: Transaction[];
+  analysisConfig: AnalysisConfig;
 }
 
 const store = createStore<State>({
   state: {
     transactions: [],
+    analysisConfig: {} as AnalysisConfig,
   },
   mutations: {
     //Commit Transaction data to store state
     setTransactions(state, transactions: Transaction[]) {
       state.transactions = transactions;
     },
+
+    //Commit analysis configuration to state
+    setAnalysisConfig(state, analysisConfig: AnalysisConfig) {
+      state.analysisConfig = analysisConfig;
+    },
   },
   actions: {
     //Retrieve Transaction data from the server
     async getTransactions({ commit }) {
       try {
-        //Fetch transactions from server and commit to store
+        //Fetch transactions from server and commit to state
         const response = await axios.get(
           'http://localhost:3000/api/transactions',
         );
@@ -42,6 +57,11 @@ const store = createStore<State>({
         console.error('Error fetching transactions:', error);
         commit('setTransactions', []);
       }
+    },
+
+    //Store changes to the analysis configuration
+    updateAnalysisConfig({ commit }, analysisConfig) {
+      commit('setAnalysisConfig', analysisConfig);
     },
   },
   getters: {
@@ -54,6 +74,32 @@ const store = createStore<State>({
         start: start,
         end: end,
       };
+    },
+
+    //Select transactions within a specified date range
+    getTransactionsByDate: (state) => (start: Date, end: Date) => {
+      return state.transactions.filter((transaction) => {
+        const transactionDate = new Date(transaction.date);
+        return transactionDate >= start && transactionDate <= end;
+      });
+    },
+
+    //return the stored analysis configuration
+    getAnalysisConfig(state) {
+      return state.analysisConfig;
+    },
+
+    //filter transactions by the parameters in  the stored analysis configuration
+    getAnalysisTransactions(state, getters) {
+      const analysisConfig = state.analysisConfig;
+      const allTransactions = getters.getTransactionsByDate(analysisConfig.startDate, analysisConfig.endDate);
+
+      return allTransactions.filter((transaction: Transaction) => {
+        const filterCategories = analysisConfig.selectedCategories.length === 0 || analysisConfig.selectedCategories.includes(transaction.category);
+        const filterSubCategories = analysisConfig.selectedSubCategories.length === 0 || analysisConfig.selectedSubCategories.includes(transaction.subCategory);
+        const filterAccounts = analysisConfig.selectedAccounts.length === 0 || analysisConfig.selectedAccounts.includes(transaction.accountName);
+        return filterCategories && filterSubCategories && filterAccounts;
+      });
     },
   },
 });

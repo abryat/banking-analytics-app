baf8b1b57faf23f8e4491438fca4685cfb118dea
abryat <ajbatkinson@gmail.com>
2025-02-21 13:14:49 +0000

    tidy quotation mark usage

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const mysql = require('mysql2');
 
 const app = express();

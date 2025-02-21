8e29fb33bfe07482cc64d3a523ee013b47fa2eb5
abryat <ajbatkinson@gmail.com>
2025-02-21 05:14:16 +0000

    add global stylesheet

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store/VuexTransactionStore';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import './assets/styles.scss';
 
 const app = createApp(App);
 app.use(router);

717dde1c2164513429246b2728d1fc96b34bee04
abryat <ajbatkinson@gmail.com>
2025-02-21 05:15:32 +0000

    style home page

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-      <h1>Banking Analytics</h1>
-      <router-link to="/setup">Get started</router-link>
-  </div>
+  <main class="home-page">
+    <section class="home-page__welcome">
+      <h1>Spending Analysis</h1>
+      <h2>Keep track of your spending</h2>
+      <hr>
+      <h3>Build customised reports to better understand your spending habits and trends over time</h3>
+      <div class="home-page__welcome__button-container">
+        <router-link to="/setup">
+        <el-button type="primary" size="large">
+          Get started
+        </el-button>
+      </router-link>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
-
 export default defineComponent({
   name: 'HomePage',
   setup() {
@@ -16,3 +26,26 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.home-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+
+    &__welcome {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      background-color: #ffffff;
+      border-radius: 1em;
+      box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+      padding: 2em;
+      text-align: center;
+      gap: 1rem;
+    }
+  }
+</style>
